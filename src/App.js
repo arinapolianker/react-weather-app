ea8f9d86be05fb4ea46cbc9c75c7f20ed0e602c1
arinapolianker <arinapolianker@gmail.com>
2022-01-28 16:16:24 +0200

    Current location weather data

--- src/App.js
+++ src/App.js
@@ -25,26 +25,22 @@ function App() {
       coords: response.data.coord,
     });
     setLoad(true);
-
-    let hour = weather.date.getHours();
-    if (hour >= 6 && hour < 7) {
+    let hours = weather.date.getHours();
+    if (hours >= 18 && hours < 6) {
+      setColor(
+        "linear-gradient(179deg, rgba(31, 58, 109, 1) 3.3%, rgba(53, 87, 150, 1) 100%)"
+      );
+    } else if (hours >= 6 && hours < 7) {
       setColor(
         "linear-gradient(179deg, rgba(138, 179, 233, 1) 3.3%, rgba(255, 159, 121, 1) 70%, rgba(252, 220, 132, 1) 100%)"
       );
-    }
-    if (hour >= 7 && hour < 16) {
+    } else if (hours >= 7 && hours < 17) {
       setColor(
         "linear-gradient(46deg, rgba(45, 130, 241, 1) 3.3%, rgba(181, 221, 247, 1) 100%)"
       );
-    }
-    if (hour >= 16 && hour < 17) {
+    } else if (hours >= 17 && hours < 18) {
       setColor(
         "linear-gradient(179deg, rgba(13, 52, 104, 1) 3.3%, rgba(255, 146, 103, 1) 70%, rgba(255, 188, 6, 1) 100%)"
-      );
-    }
-    if (hour >= 17 && hour < 6) {
-      setColor(
-        "linear-gradient(179deg, rgba(31, 58, 109, 1) 3.3%, rgba(53, 87, 150, 1) 100%)"
       );
     }
   }
@@ -56,10 +52,19 @@ function App() {
   function changeCity(event) {
     setCity(event.target.value);
   }
+  const apiKey = "089de42863cffcedc265abdd75619b42";
   function search() {
-    const apiKey = "089de42863cffcedc265abdd75619b42";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(url).then(showTemp);
+  }
+  function currentPosition(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      let positionUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+      axios.get(positionUrl).then(showTemp);
+    });
   }
 
   if (load) {
@@ -83,13 +88,12 @@ function App() {
               <button className="sub">
                 <i className="fas fa-search-location"></i>
               </button>
-              <button className="current">
+              <button className="current" onClick={currentPosition}>
                 <i className="fas fa-map-marker-alt"></i>
               </button>
             </form>
             <Weather data={weather} />
           </div>
-
           <Footer />
         </div>
       </div>
